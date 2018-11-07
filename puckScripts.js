4af3a8a6fd376c09852f2bc8a6f5c10b2dc016f4
BGuerrero825 <BGuerrero825@gmail.com>
2018-11-07 11:16:52 -0700

    lol

---
+++ puckScripts.js
@@ -0,0 +1,17 @@
+$(function() {
+  $(".item").draggable();
+  $(".slot").droppable({
+    drop: function(event, ui) {
+      var $this = $(this);
+      $this.append(ui.draggable);
+      var width = $this.width();
+      var height = $this.height();
+      var cntrLeft = (width / 2) - (ui.draggable.width() / 2);
+      var cntrTop = (height / 2) - (ui.draggable.height() / 2);
+      ui.draggable.css({
+        left: cntrLeft + "px",
+        top: cntrTop + "px"
+      });
+    }
+  });
+});
