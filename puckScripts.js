323264e842209c73c3605ac26b17fa9850b68b07
BGuerrero825 <BGuerrero825@gmail.com>
2018-11-09 10:31:52 -0700

    working on sub pucks

--- puckScripts.js
+++ puckScripts.js
@@ -15,3 +15,21 @@ $(function() {
     }
   });
 });
+
+$(function() {
+  $(".sub-item").draggable();
+  $(".item").droppable({
+    drop: function(event, ui) {
+      var $this = $(this);
+      $this.append(ui.draggable);
+      var width = $this.width();
+      var height = $this.height();
+      var cntrLeft = (width / 2) - (ui.draggable.width() / 2);
+      var cntrTop = (height / 2) - (ui.draggable.height() / 2);
+      ui.draggable.css({
+        left: cntrLeft + "px",
+        top: cntrTop + "px"
+      });
+    }
+  });
+});
