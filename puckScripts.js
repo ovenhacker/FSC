5d4c1aef8af9168999819a018d1cb6b5d237f0a9
BGuerrero825 <BGuerrero825@gmail.com>
2018-11-15 08:51:02 -0700

    working on new pucks

--- puckScripts.js
+++ puckScripts.js
@@ -1,36 +1,46 @@
-//script for drag and drop of flight pucks
-$(function() {
-  $(".item").draggable();
-  $(".slot").droppable({
-    drop: function(event, ui) {
-      var $this = $(this);
-      $this.append(ui.draggable);
-      var width = $this.width();
-      var height = $this.height();
-      var cntrLeft = (width / 2) - (ui.draggable.width() / 2);
-      var cntrTop = (height / 2) - (ui.draggable.height() / 2);
-      ui.draggable.css({
-        left: cntrLeft + "px",
-        top: cntrTop + "px"
-      });
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
+
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+      var textEl = event.target.querySelector('p');
+
+      textEl && (textEl.textContent =
+        'moved a distance of '
+        + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                     Math.pow(event.pageY - event.y0, 2) | 0))
+            .toFixed(2) + 'px');
     }
   });
-});
-//script for drag and drop of pilot pucks
-$(function() {
-  $(".sub-item").draggable();
-  $(".item").droppable({
-    drop: function(event, ui) {
-      var $this = $(this);
-      $this.append(ui.draggable);
-      var width = $this.width();
-      var height = $this.height();
-      var cntrLeft = (width / 2) - (ui.draggable.width() / 2);
-      var cntrTop = (height / 2) - (ui.draggable.height() / 2);
-      ui.draggable.css({
-        left: cntrLeft + "px",
-        top: cntrTop + "px"
-      });
-    }
-  });
-});
+
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
