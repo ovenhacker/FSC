faf77315f782ebc421b883505adb83cd800953f4
ovenhacker <morganwagner49@gmail.com>
2019-01-16 11:14:10 -0700

    Medical Pages
    Made it so the submit button pops up a little alert, this is a space holder for data to send to the database.

--- doMedicalSubmission.php
+++ doMedicalSubmission.php
@@ -1,2 +1,10 @@
 <!-- on click on submit button on medical page, this runs -->
 <!-- basically it submits the information selected in the page to the database -->
+
+<?php
+  session_start();
+  if(!isset($_SESSION['myusername'])){}
+
+
+
+ ?>
