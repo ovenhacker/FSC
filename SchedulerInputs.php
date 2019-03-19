3a441054278291370a5c1510d28b31d95b58f9c1
ovenhacker <morganwagner49@gmail.com>
2019-03-19 11:06:17 -0600

    3.21

--- SchedulerInputs.php
+++ SchedulerInputs.php
@@ -66,7 +66,7 @@
   </div>
 
   <div class="container">
-    <p>3.20</p>
+    <p>3.21</p>
     <!-- this is for testing the databse contection -->
     <!-- display of the suggestions -->
 		<table cellpadding="4">
